4852a200937c7a8bd069a13c43bf553dcbeae961
tamas-visy <60962800+tamas-visy@users.noreply.github.com>
2021-12-01 21:46:01 +0100

    Add "detector_name" to create_detector, "models" to CompositeDetector

--- src/detection/__init__.py
+++ src/detection/__init__.py
@@ -2,10 +2,11 @@ from .detector import Detector
 from .boundingbox import BoundingBox
 
 
-def create_detector(target_fps=None, use_gpu=None, **kwargs) -> Detector:
+def create_detector(target_fps=None, use_gpu=None, detector_name=None, **kwargs) -> Detector:
     """
     Creates a detector.
 
+    :param detector_name: The name of the Detector which will be used. Otherwise, a ValueError is raised.
     :param target_fps: The FPS value to be targeted when choosing the appropriate detector.
     :param use_gpu: Whether the GPU should be used, see Detector's documentation.
     :return: a Detector.
@@ -13,6 +14,15 @@ def create_detector(target_fps=None, use_gpu=None, **kwargs) -> Detector:
     from .detectors import TinyYoloV3
     from .detectors import YoloV4
     from .detectors import CompositeDetector
+
+    if detector_name is not None:
+        if detector_name == 'TinyYoloV3':
+            return TinyYoloV3(use_gpu)
+        if detector_name == 'YoloV4':
+            return YoloV4(use_gpu)
+        if detector_name == 'CompositeDetector':
+            return CompositeDetector(use_gpu)
+        raise ValueError(f'{detector_name} does not exist')
 
     if target_fps is not None:
         if target_fps < 5:
--- src/detection/detectors/composite.py
+++ src/detection/detectors/composite.py
@@ -7,9 +7,9 @@ from src.detection.detectors import YoloV4, TinyYoloV3
 
 
 class CompositeDetector(Detector):
-    def __init__(self, use_gpu=None):
+    def __init__(self, use_gpu=None, models=None):
         super().__init__(use_gpu)
-        self.models = [YoloV4(use_gpu), TinyYoloV3(use_gpu)]
+        self.models = models if models is not None else [YoloV4(use_gpu), TinyYoloV3(use_gpu)]
         self.iteration = 0
 
     def detect(self, image: np.ndarray) -> List[BoundingBox]:
